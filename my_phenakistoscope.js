6b24c2ff37103c8cfbf9d211632bf0982c747dbb
mayasydney <150874594+mayasydney@users.noreply.github.com>
2024-01-10 13:34:57 +1300

    Final Commit

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,7 +1,7 @@
 const SLICE_COUNT = 10;
 
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  pScope.output_mode(OUTPUT_PRINT(A3));
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
   pScope.set_direction(CCW);
@@ -12,38 +12,55 @@ function setup_layers(pScope){
 
   new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
 
-  var layer1 = new PLayer(faces);
+  var layer1 = new PLayer(sun);
   layer1.mode( SWIRL(5) );
   layer1.set_boundary( 200, 1000 );
 
-  var layer2 = new PLayer(squares);
+  var layer2 = new PLayer(sunrays);
   layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  layer2.set_boundary( 0, 200 );
 }
 
-function faces(x, y, animation, pScope){
+function sun(x, y, animation, pScope){
   
   scale(animation.frame*2);
 
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
+  // noticed that animation.frame is just a number between 0-1 so splitting into three parts and getting different colors that way
+  console.log(animation.frame)
+
+  if (animation.frame < 0.33) {
+    fill(255, 204, 0); // Yellow
+  }
+
+  if (animation.frame >= 0.33 && animation.frame < 0.66) {
+    fill(255, 153, 51); // Orange
+  }
+
+  if ( animation.frame >= 0.66 && animation.frame < 0.8) {
+    fill(255, 102, 0); // Darker Orange
+  }
+  ellipse(0,0,800,300); // draw sun
 
 }
 
-function squares(x, y, animation, pScope){
+function sunrays(x, y, animation, pScope){
 
   // this is how you set up a background for a specific layer
   let angleOffset = (360 / SLICE_COUNT) / 2
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
+  if (animation.frame < 0.33) {
+    stroke(255, 204, 0); // Yellow
+  }
 
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
+  if (animation.frame >= 0.33 && animation.frame < 0.66) {
+    stroke(255, 153, 51); // Orange
+  }
 
+  if ( animation.frame > 0.66) {
+    stroke(255, 102, 0); // Darker Orange
+  }
+  line(x, y, x + 400 * cos(radians(backgroundArcStart)), y + 400 * sin(radians(backgroundArcStart))); // draws sunrays
+  line(x, y, x + 400 * cos(radians(backgroundArcEnd)), y + 400 * sin(radians(backgroundArcEnd))); // draws sunrays
 }
